cc022e4446d25ed8729338dbe4574e14d93b7221
ing-brian-dev <ingbvn0@gmail.com>
2026-05-08 16:54:59 -0600

    feat: add Create and Joined Communities pages with navigation links

---
+++ app/dashboard/communities/create/page.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+import Link from "next/link";
+import Heading from "@/src/shared/components/typography/Heading";
+import { generatePageTitle } from "@/src/shared/utils/metadata";
+
+
+export const metadata: Metadata = {
+    title: generatePageTitle('Crear Comunidad')
+}
+
+export default function CreateCommunityPage() {
+    return (
+        <>
+            <Heading>Crear Comunidad</Heading>
+            <Link
+                href="/dashboard/communities"
+                className="mt-5 block lg:inline-block text-center bg-orange-500 hover:bg-orange-600 transition-colors text-xs lg:text-xl text-white py-3 px-10  font-bold"
+            >
+                Volver a mis Comunidades
+            </Link>
+        </>
+    )
+}
---
+++ app/dashboard/communities/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+import Link from "next/link";
+import Heading from "@/src/shared/components/typography/Heading";
+import { generatePageTitle } from "@/src/shared/utils/metadata";
+
+
+export const metadata: Metadata = {
+    title: generatePageTitle('Administra tus Comunidades')
+}
+
+export default function CommunitiesPage() {
+    return (
+        <>
+            <Heading>Administra tus Comunidades</Heading>
+            <div className="flex justify-between flex-col lg:flex-row">
+                <Link
+                    href="/dashboard/communities/create"
+                    className="mt-5 block lg:inline-block text-center bg-orange-500 hover:bg-orange-600 transition-colors text-xs lg:text-xl text-white py-3 px-10  font-bold"
+                >
+                    Crear Comunidad
+                </Link>
+                <Link
+                    href="/dashboard/communities/joined"
+                    className="mt-5 block lg:inline-block text-center bg-pink-500 hover:bg-pink-600 transition-colors text-xs lg:text-xl text-white py-3 px-10  font-bold"
+                >
+                    Comunidades a las que te uniste
+                </Link>
+            </div>
+        </>
+    )
+}
