631bfd295a4a81fa6d6df45ff442ac50ad0af9f4
ing-brian-dev <ingbvn0@gmail.com>
2026-05-11 13:29:57 -0600

    feat: update globals.css and next.config.ts for improved image handling and utility imports

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    typedRoutes: true,
+  typedRoutes: true,
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'tmbkk2wgvm.ufs.sh'
+    }]
+  }
 };
 
 export default nextConfig;
