03744c37efaadcc05d61f7b6535d78b7a2b51854
ing-brian-dev <ingbvn0@gmail.com>
2026-05-11 10:48:00 -0600

    feat: implement community creation form with context validation and error handling

--- src/features/communities/components/CommunityForm.tsx
+++ src/features/communities/components/CommunityForm.tsx
@@ -1,8 +1,13 @@
 "use client"
 
-import { FormInput, FormLabel, FormTextArea } from "@/src/shared/components/forms";
+import { FormError, FormInput, FormLabel, FormTextArea } from "@/src/shared/components/forms";
+import { useFormContext } from "react-hook-form";
+import { CommunityInput } from "../schemas/communitySchema";
 
 export default function CommunityForm() {
+
+    const { register, formState: { errors } } = useFormContext<CommunityInput>();
+
     return (
         <>
             <FormLabel
@@ -14,7 +19,9 @@ export default function CommunityForm() {
                 id="name"
                 type="text"
                 placeholder="Titulo Comunidad"
+                {...register('name')}
             />
+            {errors.name && <FormError>{errors.name.message}</FormError>}
             <FormLabel
                 htmlFor="description"
             >
@@ -23,8 +30,9 @@ export default function CommunityForm() {
             <FormTextArea
                 id="description"
                 placeholder="Descripción Comunidad"
-                
+                {...register('description')}
             />
+            {errors.description && <FormError>{errors.description.message}</FormError>}
         </>
     )
 }
--- src/features/communities/components/CreateCommunity.tsx
+++ src/features/communities/components/CreateCommunity.tsx
@@ -2,12 +2,35 @@
 
 import { Form, FormSubmit } from "@/src/shared/components/forms";
 import CommunityForm from "./CommunityForm";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CommunityInput, CommunitySchema } from "../schemas/communitySchema";
 
 export default function CreateCommunity() {
+
+    const methods = useForm({
+        resolver: zodResolver(CommunitySchema),
+        mode: 'all',
+        defaultValues: {
+            name: '',
+            description: ''
+        }
+    });
+    
+    const onSubmit = async (data: CommunityInput) => {
+        
+    }
+
     return (
-        <Form>
-            <CommunityForm />
-            <FormSubmit />
-        </Form>
+        <FormProvider
+            {...methods} // Context from React Hook Form
+        >
+            <Form
+                onSubmit={methods.handleSubmit(onSubmit)}
+            >
+                <CommunityForm />
+                <FormSubmit />
+            </Form>
+        </FormProvider>
     )
 }
