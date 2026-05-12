a9966e8c16c5c393996407aea2ca96a19bd67d50
ing-brian-dev <ingbvn0@gmail.com>
2026-05-12 12:23:30 -0600

    feat: implement CommunityDropdownMenu and CommunityItem components with user permissions handling

--- src/features/communities/components/MyCommunities.tsx
+++ src/features/communities/components/MyCommunities.tsx
@@ -1,14 +1,38 @@
 import { requireAuth } from "@/src/lib/auth-server"
 import { redirect } from "next/navigation";
 import { communityService } from "../services/CommunityService";
+import Link from "next/link";
+import CommunityItem from "./CommunityItem";
 
 export default async function MyCommunities() {
 
-    const {session} = await requireAuth();
-    if(!session) redirect('/auth/login');
+  const { session } = await requireAuth();
+  if (!session) redirect('/auth/login');
 
-    const communities = await communityService.getUserCommunities(session.user);
+  const communities = await communityService.getUserCommunities(session.user);
   return (
-    <div>MyCommunities</div>
+    communities.length ? (
+      <ul
+        role="list"
+        className="mt-10 shadow-lg p-10 divide-y divide-gray-100"
+      >
+        {communities.map((community) => (
+          <CommunityItem 
+            key={community.data.id}
+            community={community}
+          />
+        ))}
+      </ul>
+    ) : <p
+      className="text-center mt-10 text-lg"
+    >
+      No hay Comunidades aún
+      <Link
+        href={'/dashboard/communities/create'}
+        className="text-orange-500 font-bold"
+      >
+        Comienza Creando una.
+      </Link>
+    </p>
   )
 }
