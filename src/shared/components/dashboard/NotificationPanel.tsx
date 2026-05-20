0881d2cb64d84842659098d29cca3140a5d1a022
ing-brian-dev <ingbvn0@gmail.com>
2026-05-20 17:32:33 -0600

    feat: update community metadata title to use community name; enhance NotificationCount with Pusher integration for real-time notifications

--- src/shared/components/dashboard/NotificationPanel.tsx
+++ src/shared/components/dashboard/NotificationPanel.tsx
@@ -1,15 +1,40 @@
+import { SelectNotification } from '@/src/features/notifications/types/notification.types';
+import { useSession } from '@/src/lib/auth-client';
 import { BellIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Suspense, use } from 'react';
+import Pusher from 'pusher-js';
+import { Suspense, use, useEffect, useState } from 'react';
 
 const notificationPromise = fetch('/api/user/notifications').then(res => res.json())
 
 function NotificationCount() {
-  const totalNotifications = use(notificationPromise);
+  const unreadNotifications: number = use(notificationPromise);
+  const [totalNotifications, setTotalNotifications] = useState(unreadNotifications);
+
+  const { data } = useSession();
+
+  useEffect(() => {
+    const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
+      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!
+    });
+
+    const id = `notifications-channel-${data?.user.id}`;
+
+    const channel = pusher.subscribe(id);
+    channel.bind('new-notifications', (notification: SelectNotification) => {
+      setTotalNotifications((prev) => prev + 1);
+    });
+
+    return () => {
+      channel.unbind_all()
+      channel.unsubscribe()
+    }
+
+  }, [data]);
 
   return (
     <Link
-    href={'/dashboard/notifications'}
+      href={'/dashboard/notifications'}
       className="relative rounded-full p-1 text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500 dark:hover:text-white"
     >
       <span className="sr-only">View notifications</span>
