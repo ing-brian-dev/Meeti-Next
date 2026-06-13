e273383894fcc6f88d653bf7d6fbe1b3b7b087a4
ing-brian-dev <ingbvn0@gmail.com>
2026-06-13 13:41:00 -0600

    feat: implement ProfileForm with validation and image upload functionality

--- src/features/profile/components/ProfileForm.tsx
+++ src/features/profile/components/ProfileForm.tsx
@@ -1,30 +1,63 @@
 "use client"
 
-import { Form, FormInput, FormLabel, FormSubmit, FormTextArea } from "@/src/shared/components/forms";
+import { Form, FormError, FormInput, FormLabel, FormSubmit, FormTextArea } from "@/src/shared/components/forms";
 import UploadImage from "@/src/shared/components/upload/UploadImage";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormProvider, useForm } from "react-hook-form";
+import { ProfileInput, ProfileSchema } from "./schemas/profileSchema";
 
 
 export default function ProfileForm() {
+
+    const methods = useForm({
+        resolver: zodResolver(ProfileSchema),
+        mode: 'all',
+        defaultValues: {
+            name: '',
+            bio: '',
+            image: ''
+        }
+    });
+
+    const { register,handleSubmit, formState: { errors } } = methods;
+
+    const onSubmit = (data: ProfileInput) => {
+        
+    }
+
     return (
-        <Form>
-            <FormLabel htmlFor='name'>Nombre:</FormLabel>
-            <FormInput
-                id="name"
-                type='text'
-                placeholder='Tu Nombre'
-            />
+        <FormProvider
+            {...methods}
+        >
+            <Form
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <FormLabel htmlFor='name'>Nombre:</FormLabel>
+                <FormInput
+                    id="name"
+                    type='text'
+                    placeholder='Tu Nombre'
+                    {...register('name')}
+                />
+                {errors.name && <FormError>{errors.name.message}</FormError>}
 
-            <FormLabel id='bio'>Biografía</FormLabel>
-            <FormTextArea
-                id='bio'
-                placeholder='Añade una Descripción o Biografía'
-            />
+                <FormLabel id='bio'>Biografía</FormLabel>
+                <FormTextArea
+                    id='bio'
+                    placeholder='Añade una Descripción o Biografía'
+                    {...register('bio')}
+                />
+                {errors.bio && <FormError>{errors.bio.message}</FormError>}
 
-            <FormLabel>Imagen perfil</FormLabel>
+                <FormLabel>Imagen perfil</FormLabel>
+                <UploadImage
+                    uploadedImageLabel="Imagen Perfil"
+                />
 
-            <FormSubmit
-                value={'Guardar Cambios'}
-            />
-        </Form>
+                <FormSubmit
+                    value={'Guardar Cambios'}
+                />
+            </Form>
+        </FormProvider>
     )
 }
