7d497494df904680476b147d6ac69f3872da350e
ing-brian-dev <ingbvn0@gmail.com>
2026-06-16 13:28:55 -0600

    feat: created the profile routing

---
+++ app/(public)/profiles/[id]/page.tsx
@@ -0,0 +1,9 @@
+import Heading from "@/src/shared/components/typography/Heading";
+
+export default function ProfilePage() {
+  return (
+    <>
+        <Heading>Perfil</Heading>
+    </>
+  )
+}
--- src/shared/components/dashboard/UserMenu.tsx
+++ src/shared/components/dashboard/UserMenu.tsx
@@ -3,8 +3,9 @@ import { Bars3Icon } from "@heroicons/react/24/outline";
 import { signOut } from '@/src/lib/auth-client'
 import { redirect } from "next/navigation";
 import toast from "react-hot-toast";
+import Link from "next/link";
 
-export default function UserMenu() {
+export default function UserMenu({ userId }: { userId: string }) {
 
     const handleOnCloseSession = async () => {
         await signOut({
@@ -33,28 +34,28 @@ export default function UserMenu() {
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-black/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
             >
                 <MenuItem>
-                    <a
-                        href={`/p`}
+                    <Link
+                        href={`/profiles/${userId}`}
                         className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
                     >
                         Ver tu Perfil
-                    </a>
+                    </Link>
                 </MenuItem>
                 <MenuItem>
-                    <a
+                    <Link
                         href="/dashboard/profile"
                         className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
                     >
                         Administra tu Perfil
-                    </a>
+                    </Link>
                 </MenuItem>
                 <MenuItem>
-                    <a
+                    <Link
                         href="/dashboard/security"
                         className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
                     >
                         Seguridad
-                    </a>
+                    </Link>
                 </MenuItem>
                 <MenuItem>
                     <button
